a6b7e34b982e39e38d1fa2b51b7ccb1b9810778b
kuzmanovicd <dejank@uns.ac.rs>
2016-12-18 22:33:52 +0100

    updates

--- src/users/forms.py
+++ src/users/forms.py
@@ -3,7 +3,7 @@ from django.core import validators
 
 class RegisterForm(forms.Form):
     username = forms.CharField(max_length=24)
-    password = forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField(validators=[validators.validate_email])
     first_name = forms.CharField(max_length=24)
     last_name = forms.CharField(max_length=30)
--- src/users/views.py
+++ src/users/views.py
@@ -3,6 +3,10 @@ from django.views import View
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from .forms import RegisterForm
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+
+from .models import Gost
 
 # Create your views here.
 class Registration(View):
@@ -15,9 +19,11 @@ class Registration(View):
         }
         return render(request, 'users/register.html', context)
 
-
     def post(self, request, *args, **kwargs):
-        text = request.POST['username'] + request.POST['password']
+        form = RegisterForm(request.POST)
+        data = 'he he heheh heheh :P'
 
-        return HttpResponse(text)
+        new_user = User.objects.get_or_create(username=form.username, password=form.password, email=form.email )
+        # send_mail('Cao mala', data, 'dejan@kuzmanovicd.com', ['volaric.ana@gmail.com'])
+        return HttpResponse(data)
 
