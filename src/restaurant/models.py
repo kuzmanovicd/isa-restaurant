5a5379fffc5044f13c34d78e752055ec4d01d64b
KUZMANOVICD\deyan <dejank@uns.ac.rs>
2017-02-23 23:01:25 +0100

    Fixes.

--- src/restaurant/models.py
+++ src/restaurant/models.py
@@ -4,8 +4,5 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
 
-    description = models.CharField(max_length=120)
-    
-
     def __str__(self):
         return self.name
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -4,4 +4,4 @@ from users import models
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', '']
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
