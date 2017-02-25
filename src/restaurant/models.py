c82f8289ccc26d97f7253898bd0747b8325eede0
Nikola Spiric <nikolaspiric86@gmail.com>
2017-02-25 13:39:58 +0100

    some model

--- src/restaurant/models.py
+++ src/restaurant/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 
 # Create your models here.
+#Model restorana
+#Ima: naziv, opis, menu, konfiguraciju sedenja
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    #menu = models.OneToOneField(Food_Menu)
+    description_restaurant = models.CharField(max_length=100)
+    #menu = models.OneToOneField(Menu, on_delete=models.CASCADE)
+
+    #ovo bi trebalo biti kao za tu konfiguraciju sedenja, ali nisam siguran
+    rows = models.IntegerField()
+    columns = models.IntegerField()
+    tables = models.IntegerField()
+    is_ready = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
 
 # model stavki menu-a restorana  Dodao: Spiric
+# Ima: naziv, opis, cijenu, kolicinu, tip, id
 class MenuItem(models.Model):
     name_item = models.CharField(max_length=50)
     description_item = models.CharField(max_length=50)
@@ -21,6 +31,5 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name_item
-
 
 
