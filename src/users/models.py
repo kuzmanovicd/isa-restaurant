0d8c132b2f94d794e0506ccd911e9dd92829ba6f
Nikola Spiric <nikolaspiric86@gmail.com>
2017-02-27 01:24:20 +0100

    test

--- src/users/models.py
+++ src/users/models.py
@@ -135,7 +135,7 @@ class Bartender(Employee):
 
 
 # Model: Provider (dobavljac)
-# Polja: naziv, ostalo iz usera
+# Polja: id, naziv, user name, type, email, passwor
 # Sadrzi: --
 # Dodao: Spiric
 class Provider(BasicUser):
